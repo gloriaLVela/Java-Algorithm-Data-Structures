88666684f7d814ed0ff43328b02c32bc0d0d707c
Gloria Laura Vela <gloria.laura.vela@gmail.com>
2020-01-09 09:47:36 -0600

    reverse an array

---
+++ src/ReverseArray.java
@@ -0,0 +1,18 @@
+public class ReverseArray {
+    static int[] reverseThisArray(int[] currentArray) {
+        int tempNumber;
+        for (int i = 0; i < currentArray.length / 2; i++) {
+            tempNumber = currentArray[i];
+            currentArray[i] = currentArray[currentArray.length - (1 + i)];
+            currentArray[currentArray.length - (1 + i)] = tempNumber;
+        }
+        return currentArray;
+    }
+
+    public static void main(String[] args) {
+        int[] fixedArray = {1, 0, -2, 5, 6, -4};
+        System.out.println("fixedArray = " + fixedArray);
+        reverseThisArray(fixedArray);
+        System.out.println("fixedArray = " + fixedArray);
+    }
+}
